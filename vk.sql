156c553828a46da16e77d9adb70f88f6abf5b2e2
Igko-ai <igor_konik@mail.ru>
2021-02-14 13:16:52 +0300

    homework 3

---
+++ vk.sql
@@ -0,0 +1,135 @@
+-- Создание БД для социальной сети ВКонтакте
+-- https://vk.com/geekbrainsru
+
+-- Делаем БД текущей
+USE vk;
+
+-- Создаём таблицу пользователей
+CREATE TABLE users (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Идентификатор строки", 
+  first_name VARCHAR(100) NOT NULL COMMENT "Имя пользователя",
+  last_name VARCHAR(100) NOT NULL COMMENT "Фамилия пользователя",
+  email VARCHAR(100) NOT NULL UNIQUE COMMENT "Почта",
+  phone VARCHAR(100) NOT NULL UNIQUE COMMENT "Телефон",
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки",  
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "Время обновления строки"
+) COMMENT "Пользователи";  
+
+-- Таблица профилей
+CREATE TABLE profiles (
+  user_id INT UNSIGNED NOT NULL PRIMARY KEY COMMENT "Ссылка на пользователя", 
+  gender CHAR(1) NOT NULL COMMENT "Пол",
+  birthday DATE COMMENT "Дата рождения",
+  photo_id INT UNSIGNED COMMENT "Ссылка на основную фотографию пользователя",
+  status VARCHAR(30) COMMENT "Текущий статус",
+  city VARCHAR(100) COMMENT "Город проживания",
+  postcode VARCHAR(100) COMMENT "Регион проживания",
+  country_id INT UNSIGNED COMMENT "Ссылка на страну проживания",
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки",  
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "Время обновления строки"
+) COMMENT "Профили"; 
+
+-- Таблица-справочник стран
+CREATE TABLE countries (
+ id INT UNSIGNED NOT NULL PRIMARY KEY COMMENT "Цифровой код страны",
+ country VARCHAR(50) COMMENT "Название страны"
+) COMMENT "Страны";
+
+-- Таблица сообщений
+CREATE TABLE messages (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Идентификатор строки", 
+  from_user_id INT UNSIGNED NOT NULL COMMENT "Ссылка на отправителя сообщения",
+  to_user_id INT UNSIGNED NOT NULL COMMENT "Ссылка на получателя сообщения",
+  body TEXT NOT NULL COMMENT "Текст сообщения",
+  is_important BOOLEAN COMMENT "Признак важности",
+  is_delivered BOOLEAN COMMENT "Признак доставки",
+  created_at DATETIME DEFAULT NOW() COMMENT "Время создания строки",
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "Время обновления строки"
+) COMMENT "Сообщения";
+
+-- Таблица дружбы
+CREATE TABLE friendship (
+  user_id INT UNSIGNED NOT NULL COMMENT "Ссылка на инициатора дружеских отношений",
+  friend_id INT UNSIGNED NOT NULL COMMENT "Ссылка на получателя приглашения дружить",
+  status_id INT UNSIGNED NOT NULL COMMENT "Ссылка на статус (текущее состояние) отношений",
+  requested_at DATETIME DEFAULT NOW() COMMENT "Время отправления приглашения дружить",
+  confirmed_at DATETIME COMMENT "Время подтверждения приглашения",
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки",  
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "Время обновления строки",  
+  PRIMARY KEY (user_id, friend_id) COMMENT "Составной первичный ключ"
+) COMMENT "Таблица дружбы";
+
+-- Таблица статусов дружеских отношений
+CREATE TABLE friendship_statuses (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Идентификатор строки",
+  name VARCHAR(150) NOT NULL UNIQUE COMMENT "Название статуса",
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки",  
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "Время обновления строки"  
+) COMMENT "Статусы дружбы";
+
+-- Таблица групп
+CREATE TABLE communities (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Идентификатор сроки",
+  name VARCHAR(150) NOT NULL UNIQUE COMMENT "Название группы",
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки",  
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "Время обновления строки"  
+) COMMENT "Группы";
+
+-- Таблица связи пользователей и групп
+CREATE TABLE communities_users (
+  community_id INT UNSIGNED NOT NULL COMMENT "Ссылка на группу",
+  user_id INT UNSIGNED NOT NULL COMMENT "Ссылка на пользователя",
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки", 
+  PRIMARY KEY (community_id, user_id) COMMENT "Составной первичный ключ"
+) COMMENT "Участники групп, связь между пользователями и группами";
+
+-- Таблица медиафайлов
+CREATE TABLE media (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Идентификатор строки",
+  user_id INT UNSIGNED NOT NULL COMMENT "Ссылка на пользователя, который загрузил файл",
+  filepath VARCHAR(255) NOT NULL COMMENT "Путь к файлу",
+  size INT NOT NULL COMMENT "Размер файла",
+  metadata JSON COMMENT "Метаданные файла",
+  media_type_id INT UNSIGNED NOT NULL COMMENT "Ссылка на тип контента",
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки",
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "Время обновления строки"
+) COMMENT "Медиафайлы";
+
+-- Таблица типов медиафайлов
+CREATE TABLE media_types (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Идентификатор строки",
+  name VARCHAR(255) NOT NULL UNIQUE COMMENT "Название типа",
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки",  
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "Время обновления строки"
+) COMMENT "Типы медиафайлов";
+
+-- Таблица типов лайков
+CREATE TABLE like_types (
+ id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Идетификатор строки",
+ like_type VARCHAR(20) NOT NULL UNIQUE COMMENT "Тип лайка"
+) COMMENT "Реакция";
+
+-- Таблица связи пользователей и лайков
+CREATE TABLE users_likes (
+  like_type_id INT UNSIGNED NOT NULL COMMENT "Ссылка на реакцию",
+  user_id INT UNSIGNED NOT NULL COMMENT "Ссылка на пользователя",
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки", 
+  PRIMARY KEY (like_type_id, user_id) COMMENT "Составной первичный ключ"
+) COMMENT "Реакция на пользователя";
+
+-- Таблица связи постов и лайков
+CREATE TABLE messages_likes (
+  like_type_id INT UNSIGNED NOT NULL COMMENT "Ссылка на реакцию",
+  message_id INT UNSIGNED NOT NULL COMMENT "Ссылка на пост",
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки", 
+  PRIMARY KEY (like_type_id, message_id) COMMENT "Составной первичный ключ"
+) COMMENT "Реакция на пост";
+
+-- Таблица связи медиафайлов и лайков
+CREATE TABLE media_likes (
+  like_type_id INT UNSIGNED NOT NULL COMMENT "Ссылка на реакцию",
+  media_id INT UNSIGNED NOT NULL COMMENT "Ссылка на медиа",
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки", 
+  PRIMARY KEY (like_type_id, media_id) COMMENT "Составной первичный ключ"
+) COMMENT "Реакция на медиафайл";
+
